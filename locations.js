1285d18a77c31193a4a5ffa3c3d6752589489155
heartbeat4-25 <145019316+heartbeat4-25@users.noreply.github.com>
2025-06-01 23:36:31 +0800

    Create locations.js

---
+++ locations.js
@@ -0,0 +1,9 @@
+// locations.js
+
+const districtData = [
+  { name: "吴兴区", lng: 120.1881, lat: 30.8610, type: "district" },
+  { name: "南浔区", lng: 120.4130, lat: 30.8695, type: "district" },
+  { name: "妙西镇", lng: 119.8710, lat: 30.8032, type: "street" },
+  { name: "道场乡", lng: 120.0576, lat: 30.7804, type: "street" },
+  // ... 你将来会加的 200+ 数据
+];
