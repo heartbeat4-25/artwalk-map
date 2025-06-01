1fdb353fedb5e2a06062825499742657c49044c5
heartbeat4-25 <145019316+heartbeat4-25@users.noreply.github.com>
2025-06-01 23:48:16 +0800

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,9 +1,37 @@
-// locations.js
-
-const districtData = [
-  { name: "吴兴区", lng: 120.1881, lat: 30.8610, type: "district" },
-  { name: "南浔区", lng: 120.4130, lat: 30.8695, type: "district" },
-  { name: "妙西镇", lng: 119.8710, lat: 30.8032, type: "street" },
-  { name: "道场乡", lng: 120.0576, lat: 30.7804, type: "street" },
-  // ... 你将来会加的 200+ 数据
-];
+    const districtData = [
+      {
+        name: "吴兴区",
+        lng: 120.1881,
+        lat: 30.8610,
+        desc: "吴兴区是湖州市中心城区，经济、文化、交通中心。拥有太湖旅游度假区、织里童装等特色产业。",
+        image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/wuxing.jpg"
+      },
+      {
+        name: "南浔区",
+        lng: 120.4130,
+        lat: 30.8695,
+        desc: "南浔区以南浔古镇著称，是国家5A级景区，典型的江南水乡，文化底蕴深厚。",
+        image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/nanxun.jpg"
+      },
+      {
+        name: "德清县",
+        lng: 119.9772,
+        lat: 30.5346,
+        desc: "德清县地处杭嘉湖平原西部，拥有莫干山国家旅游度假区，是长三角宜居县城代表。",
+        image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/deqing.jpg"
+      },
+      {
+        name: "长兴县",
+        lng: 119.9076,
+        lat: 31.0261,
+        desc: "长兴县位于湖州西北部，太湖西南岸，是国家生态县，产业结构较完善。",
+        image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/changxing.jpg"
+      },
+      {
+        name: "安吉县",
+        lng: 119.6806,
+        lat: 30.6389,
+        desc: "安吉县是中国竹乡，以绿色发展著称，曾获联合国人居奖，生态环境优越。",
+        image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/anjicounty.jpg"
+      }
+    ];
