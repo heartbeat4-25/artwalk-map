fa450e88f117ed8ffe053301727b6470e7716b6b
heartbeat4-25 <daring961@outlook.com>
2025-07-08 23:30:39 +0800

    change locations

---
+++ locations_anxi.js
@@ -0,0 +1,128 @@
+const districtData = [
+  {
+    name: "五峰山茶馆",
+    lng: 118.183973,
+    lat: 25.027294,
+    desc: "位于安溪凤山绿道附近，环境幽静，是当地颇受欢迎的休闲茶馆。",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/wufeng1.png"
+  },
+  {
+    name: "清隐茶馆",
+    lng: 118.189509,
+    lat: 25.065298,
+    desc: "隐于凤山绿道，主打围炉煮茶、罐罐烤奶等特色茶饮，假期人气爆棚，是安溪夜间经济代表之一。:contentReference[oaicite:1]{index=1}",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/qingyin1.png"
+  },
+  {
+    name: "将相和山庄",
+    lng: 118.210572,
+    lat: 25.066016,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/jiangxianghe1.png"
+  },
+  {
+    name: "芳松里小院",
+    lng: 118.20679199999995,
+    lat: 25.0252,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/fangsongli1.png"
+  },
+  {
+    name: "静心茶馆(安溪凤山风景旅游区店)",
+    lng: 118.185227,
+    lat: 25.070434,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/jingxin1.png"
+  },
+  {
+    name: "冠和茶馆",
+    lng: 118.188442,
+    lat: 25.04341,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/guanhe1.png"
+  },
+  {
+    name: "茶香小院",
+    lng: 118.206529,
+    lat: 25.022184,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/chaxiang1.png"
+  },
+  {
+    name: "阅谈共享茶室",
+    lng: 118.175107,
+    lat: 25.059893,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/yuedu1.png"
+  },
+  {
+    name: "七号茶楼",
+    lng: 118.159291,
+    lat: 25.062268,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/qihao1.png"
+  },
+  {
+    name: "清溪茶驿·龙湖",
+    lng: 118.189462,
+    lat: 25.049323,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/qingxi1.png"
+  },
+  {
+    name: "孔雀山庄",
+    lng: 118.168691,
+    lat: 25.041245,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/kongque1.png"
+  },
+  {
+    name: "渝风堂",
+    lng: 118.186414,
+    lat: 25.027414,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/yufeng1.png"
+  },
+  {
+    name: "八马茶业",
+    lng: 118.093979,
+    lat: 24.986503,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/bama1.png"
+  },
+  {
+    name: "溪禾老茶馆",
+    lng: 118.210296,
+    lat: 25.085068,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/xihe1.png"
+  },
+  {
+    name: "约茶吧",
+    lng: 118.155542,
+    lat: 25.067027,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/yuecha1.png"
+  },
+  {
+    name: "九玖茶舍",
+    lng: 118.103472,
+    lat: 25.024707,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/jiujiu1.png"
+  },
+  {
+    name: "阆苑岩茶馆",
+    lng: 118.155683,
+    lat: 25.032282,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/langyuan1.png"
+  },
+  {
+    name: "素音茶舍",
+    lng: 118.18815199999995,
+    lat: 25.06227,
+    desc: "",
+    image: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/cards/anxi/suyin1.png"
+  }
+];
