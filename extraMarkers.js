f17937240eb1d8aab26ca25f1e5c924aa54b4f30
heartbeat4-25 <daring961@outlook.com>
2025-08-10 15:48:54 +0800

    detailmaps

---
+++ extraMarkers.js
@@ -0,0 +1,35 @@
+// extraMarkers.js
+window.extraImageLayersData = [
+  {
+    name: "芳松里小院",
+    lng: 118.206792,
+    lat: 25.0252,
+    url: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/maps/fangsongli.png",
+    widthMeters: 5000,   // 覆盖宽度，按需调大/调小
+    zIndex: 8
+  },
+  {
+    name: "华祥苑茶庄园",
+    lng: 117.68023199999993,
+    lat: 24.964947999999996,
+    url: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/maps/huaxiang.png",
+    widthMeters: 5000,   // 覆盖宽度，按需调大/调小
+    zIndex: 8
+  },
+  {
+    name: "高雄建发茶庄园",
+    lng: 117.92513000000008,
+    lat: 24.94309800000001,
+    url: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/maps/gaoxiong.png",
+    widthMeters: 5000,   // 覆盖宽度，按需调大/调小
+    zIndex: 8
+  },
+  {
+    name: "溪禾山铁观音文化园",
+    lng: 118.20924500000001,
+    lat: 25.083272000000004,
+    url: "https://raw.githubusercontent.com/heartbeat4-25/artwalk-map/main/images/maps/xihe.png",
+    widthMeters: 5000,   // 覆盖宽度，按需调大/调小
+    zIndex: 8
+  },
+];
